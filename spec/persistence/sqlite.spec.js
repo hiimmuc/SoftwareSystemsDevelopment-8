6d8fa2ddca8bc3e6c055434d1ccfc3fb8404b353
hiimmuc <nam1811.hust@gmail.com>
2024-01-29 02:45:52 +0700

    change Dockerfile

--- spec/persistence/sqlite.spec.js
+++ spec/persistence/sqlite.spec.js
@@ -1,5 +1,6 @@
 const db = require('../../src/persistence/sqlite');
 const fs = require('fs');
+const location = process.env.SQLITE_DB_LOCATION || '/etc/todos/todo.db';
 
 const ITEM = {
     id: '7aef3d7c-d301-4846-8358-2a91ec9d6be3',
@@ -8,8 +9,8 @@ const ITEM = {
 };
 
 beforeEach(() => {
-    if (fs.existsSync('/etc/todos/todo.db')) {
-        fs.unlinkSync('/etc/todos/todo.db');
+    if (fs.existsSync(location)) {
+        fs.unlinkSync(location);
     }
 });
 
@@ -45,6 +46,16 @@ test('it can update an existing item', async () => {
     expect(items[0].completed).toBe(!ITEM.completed);
 });
 
+
+
+test('it can get a single item', async () => {
+    await db.init();
+    await db.storeItem(ITEM);
+
+    const item = await db.getItem(ITEM.id);
+    expect(item).toEqual(ITEM);
+});
+
 test('it can remove an existing item', async () => {
     await db.init();
     await db.storeItem(ITEM);
@@ -53,12 +64,4 @@ test('it can remove an existing item', async () => {
 
     const items = await db.getItems();
     expect(items.length).toBe(0);
-});
-
-test('it can get a single item', async () => {
-    await db.init();
-    await db.storeItem(ITEM);
-
-    const item = await db.getItem(ITEM.id);
-    expect(item).toEqual(ITEM);
 });
